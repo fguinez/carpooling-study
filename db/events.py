f6bcb303511460d624ccc9f85565a09fedda887e
Francisco Guíñez <fhguinez@uc.cl>
2023-01-15 16:55:20 -0300

    build: update requirements

--- db/events.py
+++ db/events.py
@@ -10,9 +10,15 @@ env = dotenv_values(".env")
 
 async def connect(app: FastAPI) -> None:
     logger.info("Connecting to {0} ...", repr(env.DATABASE_URL))
-    app.state.pool = await aiomysql.create_pool(host=env.HOST, port=env.PORT,
-                                                user=env.USER, password=env.PWD,
-                                                db=env.DB, loop=None, autocommit=False)
+    app.state.pool = await aiomysql.create_pool(
+        host=env.MYSQL_HOST,
+        port=env.MYSQL_PORT,
+        user=env.MYSQL_USER,
+        password=env.MYSQL_ROOT_PASSWORD,
+        db=env.MYSQL_DB,
+        loop=None,
+        autocommit=False
+    )
     logger.info("DB: Connection established")
 
 
