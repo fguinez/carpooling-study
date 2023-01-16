8b6a6f437eda0b419eea0af96703c00c5020cb00
Francisco Guíñez <fhguinez@uc.cl>
2023-01-16 19:52:42 -0300

    fix: handle premature connection to db

--- utils/handler.py
+++ utils/handler.py
@@ -1,12 +1,21 @@
 # Based on https://github.com/xiaozl/fastapi-realworld-example-app-mysql
 import db.events as db
+import time
+
 from fastapi import FastAPI
+from loguru import logger
 from typing import Callable
 
 
 def create_startup(app: FastAPI) -> Callable:
     async def startup() -> None:
-        await db.connect(app)
+        try:
+            await db.connect(app)
+        except Exception as e:
+            logger.error(e)
+            logger.info("Waiting database startup...")
+            time.sleep(15)
+            await db.connect(app)
     return startup
 
 
